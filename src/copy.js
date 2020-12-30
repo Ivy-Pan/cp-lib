3c97d8577477601f015ad5ac709eb863872bdde6
Ivy-Pan <643936369@qq.com>
2020-12-30 22:22:56 +0800

    add copy()

--- src/copy.js
+++ src/copy.js
@@ -1,9 +1,64 @@
 import typeOf from './typeOf';
 
-function copy(param, isDeep) {
-  const type = typeOf(param);
+const BASE_COPY = {
+  number: num => num,
+  string: str => str,
+  boolean: bool => bool,
+  symbol: sb => sb,
+  function: fn => fn,
+  undefined: () => undefined,
+  null: () => null,
+  NaN: () => NaN,
+  infinity: () => Infinity,
+  '-infinity': () => -Infinity,
+  date: date => new Date(date),
+  error: err => new Error(err.message),
+  regExp: reg => new RegExp(reg),
+  // Array, Object, Set, Map
+};
 
-  console.log(type, isDeep);
-}
+const COMPLEX_COPY = {
+  object: (data, isDeep, copy) => {
+    if (isDeep) {
+      const dup = {};
+
+      Object.entries(([key, value]) => {
+        dup[key] = copy(value, isDeep);
+      });
+
+      return dup;
+    }
+
+    return { ...data };
+  },
+  map: () => {},
+  array: (data, isDeep, copy) => {
+    if (isDeep) return data.map(item => copy(item, isDeep));
+
+    return [...data];
+  },
+  set: (data, isDeep, copy) => {
+    if (isDeep) {
+      const dup = [];
+
+      data.forEach(item => {
+        dup.push(copy(item, isDeep));
+      });
+
+      return dup;
+    }
+
+    return [...data];
+  },
+};
+
+const copy = (data, isDeep) => {
+  const type = typeOf(data);
+  const baseCopy = BASE_COPY[type];
+
+  if (baseCopy) return baseCopy(data);
+
+  return COMPLEX_COPY[type](data, isDeep, copy);
+};
 
 export default copy;
