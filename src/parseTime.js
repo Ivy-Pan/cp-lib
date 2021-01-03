d55ea9ae7f825cc45c5fd91fa77289617e0f60f2
Ivy-Pan <643936369@qq.com>
2021-01-03 20:15:01 +0800

    parseTime

---
+++ src/parseTime.js
@@ -0,0 +1,34 @@
+/**
+ * parse time to formatter string
+ */
+function parseTime(datetime, formatter = 'yyyy-MM-dd hh:mm:ss') {
+  if (!datetime) return null;
+
+  const _datetime = new Date(datetime);
+  const _formatter = {
+    y: _datetime.getFullYear(),
+    M: _datetime.getMonth() + 1,
+    d: _datetime.getDate(),
+    h: _datetime.getHours(),
+    m: _datetime.getMinutes(),
+    s: _datetime.getSeconds(),
+    w: _datetime.getDay(),
+  };
+  const timeStr = formatter.replace(/([yMdhmsw]+)/g, (result, key) => {
+    const _key = key[0];
+    const value = `${_formatter[_key]}`;
+
+    if (_key === 'w') return ['日', '一', '二', '三', '四', '五', '六'][value];
+
+    return [...key].reduceRight((_timerStr, item, index) => {
+      // eslint-disable-next-line no-param-reassign
+      _timerStr += value[index];
+
+      return _timerStr;
+    }, '');
+  });
+
+  return timeStr;
+}
+
+export default parseTime;
