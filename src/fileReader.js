dec71a12bb08bd54bb03f8a30dce996a818df169
Ivy <643936369@qq.com>
2021-03-04 17:47:04 +0800

    merge

---
+++ src/fileReader.js
@@ -0,0 +1,10 @@
+/**
+ * read Blob or File
+ * @param {*} params
+ * @return {String} url
+ * readyState、onload
+ * dataUrl(base64)
+ */
+// function fileReader(blob) {
+//   const reader = new FileReader();
+// }
