095e2bd0c99555490cc86ff378ebeba4d2bba1cd
Ivy <643936369@qq.com>
2021-03-27 18:07:30 +0800

    watcher

--- src/watcher.js
+++ src/watcher.js
@@ -1,45 +1,41 @@
-const _Watcher = (function watch() {
-  let watcher = null;
+const watcher = {};
 
-  return class Watcher {
-    constructor() {
-      // 单例模式
-      if (watcher) return watcher;
+class Watcher {
+  constructor(name) {
+    // 单例模式
+    if (watcher[name]) return watcher[name];
 
-      this._name = 'WATCHER';
-      this._queue = {};
-      watcher = this;
+    this._name = name;
+    this._queue = {};
+    watcher[name] = this;
+  }
+
+  on(name, cb) {
+    this._queue[name] = this._queue[name] || [];
+    this._queue[name].push(cb);
+  }
+
+  emit(name, ...args) {
+    const handlers = this._queue[name];
+
+    if (handlers) {
+      handlers.forEach(handler => {
+        handler.call(this, ...args);
+      });
     }
+  }
 
-    on(name, cb) {
-      console.log(name);
-      this._queue[name] = this._queue[name] || [];
-      this._queue[name].push(cb);
-    }
+  remove(name, fnName) {
+    if (name && this._queue[name]) {
+      if (fnName === undefined || fnName === null) {
+        delete this._queue[name];
+      } else {
+        const index = this._queue[name].find(fn => fn.name === fnName);
 
-    emit(name, ...args) {
-      const handlers = this._queue[name];
-
-      if (handlers) {
-        handlers.forEach(handler => {
-          console.log(...args);
-          handler.call(this, ...args);
-        });
+        if (index > -1) this._queue[name].splice(index, 1);
       }
     }
+  }
+}
 
-    remove(name, fnName) {
-      if (name && this._queue[name]) {
-        if (fnName === undefined || fnName === null) {
-          delete this._queue[name];
-        } else {
-          const index = this._queue[name].find(fn => fn.name === fnName);
-
-          if (index > -1) this._queue[name].splice(index, 1);
-        }
-      }
-    }
-  };
-}());
-
-export default _Watcher;
+export default Watcher;
